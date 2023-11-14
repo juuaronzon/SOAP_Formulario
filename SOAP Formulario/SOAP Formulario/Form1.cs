64b27084452030e126166667a3ed75b95ac3dd24
juuaronzon <131707113+juuaronzon@users.noreply.github.com>
2023-11-14 10:21:06 -0300

    Add files via upload

---
+++ SOAP Formulario/SOAP Formulario/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace FORMSOAP
+
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SOAP.TipoCambio client = new SOAP.TipoCambio();
+
+            var respuesta = client.TipoCambioDia();
+
+            txt_1.Text=(respuesta.CambioDolar.First().fecha);
+
+            txt_2.Text=(respuesta.CambioDolar.First().referencia).ToString();
+        }
+    }
+}
